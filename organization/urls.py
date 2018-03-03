0777786c94c696a668b41ea25ed4d29386c63661
chenjobin <923869988@qq.com>
2018-03-03 17:22:21 +0800

    V0.14 界面修改，发现推荐机构这个显示有问题，课程列表还没做完

--- organization/urls.py
+++ organization/urls.py
@@ -8,12 +8,12 @@ from .views import TeacherListView, TeacherDetailView
 app_name = 'organization'    #django2.0需要加上，否则报错
 urlpatterns = [
     #课程机构列表页
-    path(r'list/', OrgView.as_view(), name="org_list"),
-    path(r'add_ask/', AddUserAskView.as_view(), name="add_ask"),
-    path(r'home/<int:org_id>/', OrgHomeView.as_view(), name="org_home"),
-    path(r'course/<int:org_id>/', OrgCourseView.as_view(), name="org_course"),
-    path(r'desc/<int:org_id>/', OrgDescView.as_view(), name="org_desc"),
-    path(r'org_teacher/<int:org_id>/', OrgTeacherView.as_view(), name="org_teacher"),
+    path('list/', OrgView.as_view(), name="org_list"),
+    path('add_ask/', AddUserAskView.as_view(), name="add_ask"),
+    path('home/<int:org_id>/', OrgHomeView.as_view(), name="org_home"),
+    path('course/<int:org_id>/', OrgCourseView.as_view(), name="org_course"),
+    path('desc/<int:org_id>/', OrgDescView.as_view(), name="org_desc"),
+    path('org_teacher/<int:org_id>/', OrgTeacherView.as_view(), name="org_teacher"),
     #
     #机构收藏
     path(r'add_fav/', AddFavView.as_view(), name="add_fav"),
