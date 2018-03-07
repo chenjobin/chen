62bf1bef122080b3f32f368af3e9e06d88b1c648
chenjobin <923869988@qq.com>
2018-03-07 16:50:31 +0800

    V0.14.7 首页，图片的适配要考虑好

--- courses/models.py
+++ courses/models.py
@@ -8,11 +8,11 @@ from organization.models import CourseOrg, Teacher
 class Course(models.Model):
     course_org = models.ForeignKey(CourseOrg, verbose_name=u"课程机构", null=True, blank=True,on_delete=models.CASCADE)
     name = models.CharField(max_length=50, verbose_name=u"课程名")
+    is_banner = models.BooleanField(default=False, verbose_name=u"是否放在首页")   #改成首页课程
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     detail = UEditorField(verbose_name=u"课程详情", height=300, width=1000,
                  default=u'', blank=True, imagePath="uploads/images/",
                  toolbars='besttome', filePath='uploads/files/')
-    is_banner = models.BooleanField(default=False, verbose_name=u"是否轮播")
     teacher = models.ForeignKey(Teacher, verbose_name=u"讲师", null=True, blank=True,on_delete=models.CASCADE)
     degree = models.CharField(verbose_name=u"难度", choices=(("cj","初级"), ("zj","中级"), ("gj","高级")), max_length=2)
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
@@ -52,10 +52,10 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
-
+# 本是轮播课程，因需要，变首页课程，变相置顶
 class BannerCourse(Course):
     class Meta:
-        verbose_name = "轮播课程"
+        verbose_name = "首页课程"
         verbose_name_plural = verbose_name
         proxy = True
 
--- users/views.py
+++ users/views.py
@@ -37,13 +37,13 @@ class IndexView(View):
     def get(self, request):
         #取出轮播图
         all_banners = Banner.objects.all().order_by('index')
-        courses = Course.objects.filter(is_banner=False)[:6]
-        banner_courses = Course.objects.filter(is_banner=True)[:3]
+        courses = Course.objects.filter(is_banner=True).order_by('-add_time')[:5]    #变相的置顶课程
+        # banner_courses = Course.objects.filter(is_banner=True)[:3]
         course_orgs = CourseOrg.objects.all()[:15]
         return render(request, 'index.html', {
             'all_banners':all_banners,
             'courses':courses,
-            'banner_courses':banner_courses,
+            # 'banner_courses':banner_courses,
             'course_orgs':course_orgs
         })
 
